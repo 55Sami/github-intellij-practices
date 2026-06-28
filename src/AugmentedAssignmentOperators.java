44bc6f3ddcfca4e19a9253566f2056301207f272
samikshyaghimire <samikshyaghimire2058@gmail.com>
2026-06-29 07:10:13 +0900

    Arithmetic Operators

---
+++ src/AugmentedAssignmentOperators.java
@@ -0,0 +1,15 @@
+public class AugmentedAssignmentOperators{
+    public static void main(String[] args){
+        int x= 15;
+        int y=5;
+
+        //x+=y;
+        //x-=y;
+        //x*=y;
+        //x/=y;
+        x%=y;
+
+
+        System.out.println(x);
+    }
+}
