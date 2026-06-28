3b2f3d8d35e8cd0018ad4b4bef40d00167b0ff26
samikshyaghimire <samikshyahimire2058@gamil.com>
2026-06-29 06:59:02 +0900

    ArithmeticOperators

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,17 @@
+
+
+public class ArithmeticOperators {
+    public static void main(String[]args){
+
+        int x= 10;
+        int y= 3;
+        int g = x+y;
+        int W = x*y;
+        int q = x%y;
+        int r= x-y;
+        int d = x/y;
+
+        System.out.println(q);
+
+    }
+}
