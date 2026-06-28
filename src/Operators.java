531bdca4ee9c18e00f183bf894613e7225f568e4
samikshyaghimire <samikshyahimire2058@gamil.com>
2026-06-29 07:16:09 +0900

    increment and decrement operators

---
+++ src/Operators.java
@@ -0,0 +1,12 @@
+public class Operators {
+    public static void main(String[] args){
+
+        // Increment and Decrement Operators
+
+        int x=1;
+        x++;    // adds 1 to x every time
+        x--;    //decreases value of x by one
+        System.out.println(x);
+
+    }
+}
