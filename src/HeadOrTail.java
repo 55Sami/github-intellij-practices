c0954d605b55717e0c83ece4a306eed782271ad8
samikshyaghimire <samikshyahimire2058@gamil.com>
2026-06-30 04:23:04 +0900

    Head and tails

--- src/HeadOrTail.java
+++ src/HeadOrTail.java
@@ -4,11 +4,11 @@ public class HeadOrTail {
     public static void main(String[] args){
         Random random = new Random();
 
-        boolean isHead =random.nextBoolean();
+        boolean isHead1 =random.nextBoolean();
 
         //System.out.println(isHead);
 
-        if(isHead){
+        if(isHead1){
             System.out.println("it's a HEAD");
         }else{
             System.out.println("it's a TAIL");
