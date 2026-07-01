1faf16f096c656d40f41d300e5807c326684380c
samikshyaghimire <samikshyahimire2058@gamil.com>
2026-07-02 00:02:46 +0900

    ternary operators

---
+++ src/TerneryOperator2.java
@@ -0,0 +1,9 @@
+public class TerneryOperator2 {
+    public static void main(String[] args) {
+
+        int hours = 18;
+
+        String timeOfDay =(hours < 12) ? "A.M" : "P>M";
+        System.out.println(timeOfDay);
+    }
+}
