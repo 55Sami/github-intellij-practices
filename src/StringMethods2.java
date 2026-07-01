615e3f3b8725038628a4ab1cb99991a61b953be6
samikshyaghimire <samikshyahimire2058@gamil.com>
2026-07-01 21:37:21 +0900

    String Methods

---
+++ src/StringMethods2.java
@@ -0,0 +1,15 @@
+public class StringMethods2 {
+    public static void main(String[] args) {
+        String name = "Password";
+
+
+        if(name.equalsIgnoreCase("password")){
+            System.out.println("Your name cannot be password");
+        }else{
+            System.out.println("hello "+name);
+        }
+
+    }
+
+
+}
