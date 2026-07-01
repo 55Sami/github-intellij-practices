f1466911de028abd13938f09385a572a4eb41b5f
samikshyaghimire <samikshyahimire2058@gamil.com>
2026-07-01 23:50:00 +0900

    ternary operator

---
+++ src/TerneryOperator.java
@@ -0,0 +1,9 @@
+public class TerneryOperator {
+    public static void main(String[] args) {
+        int score = 30;
+
+        String passOrFail = (score >= 60) ? "PASS":"FAIL";
+
+        System.out.println(passOrFail);
+    }
+}
