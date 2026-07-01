9db4f91d0d6512941a22a4c511d90c9e0fb721ea
samikshyaghimire <samikshyaghimire2058@gmail.com>
2026-07-01 18:38:53 +0900

    Bank Loan Approval System

---
+++ src/StringMethods.java
@@ -0,0 +1,35 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String name = "Namjoon";
+
+        if(name.contains(" ")){
+            System.out.println("Your name contains a space");
+        }else{
+            System.out.println("Your name does not contain spaces");
+        }
+
+        //int length = name.length();
+        //char letter =name.charAt(0);
+        //int index = name.indexOf("o");
+        //int lastIndex = name.lastIndexOf("o");
+
+       // name= name.toUpperCase();
+       // name = name.toLowerCase();
+      //  name = name.trim();      //removes spaces
+      //  name = name.replace("o","i");    //replaces the specific letter
+
+
+
+
+
+       /* System.out.println(length);
+        System.out.println(letter);
+        System.out.println(index);     //prints space if there is space in the string input
+        System.out.println(lastIndex);
+        System.out.println(name);
+        System.out.println(name.isEmpty());      //check if variable is empty or not*/
+
+    }
+
+
+}
