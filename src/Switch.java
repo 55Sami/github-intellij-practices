807c670cbac7e5d2c5f3dc2be95e3b05bdf3226a
samikshyaghimire <samikshyaghimire2058@gmail.com>
2026-07-02 00:19:33 +0900

    swith

--- src/Switch.java
+++ src/Switch.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Switch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String day;
+        System.out.print("Enter the day of the week: " );
+        day = scanner.nextLine();
+
+        switch(day){
+            case "Monday" ,"Tuesday" ,"Wednesday" ,"Thursday" ,"Friday" ->
+                    System.out.println("It is a weekday😫😫");
+            case "Saturday","Sunday" ->
+                    System.out.println("It is a weekends🤩🤩");
+            default -> System.out.println(day + "is not the valid day");
+
+        }
+        scanner.close();
+    }
 }
