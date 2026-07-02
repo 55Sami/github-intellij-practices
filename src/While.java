7f28d424989a8f889b8a4232bd13016bc877eeef
samikshyaghimire <samikshyaghimire2058@gmail.com>
2026-07-03 00:20:20 +0900

    while loop

--- src/While.java
+++ src/While.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class While {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String name ="";
+
+        while(name.isEmpty()){
+            System.out.print("Enter Your Name: ");
+            name = scanner.nextLine();
+        }
+        System.out.println("Hello "+name);
+
+        scanner.close();
+    }
 }
