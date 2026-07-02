b156a91a8e2defc9aa608ab67c0d0b86d4a9071f
samikshyaghimire <samikshyaghimire2058@gmail.com>
2026-07-03 00:53:00 +0900

    do while

---
+++ src/DoWhile.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num =0;
+        do{
+            System.out.print("Enter the number between 1 to 10: ");
+            num = scanner.nextInt();
+        }
+        while(num<1 || num>10);
+
+        System.out.println("You picked " + num);
+        scanner.close();
+
+    }
+}
+
+
