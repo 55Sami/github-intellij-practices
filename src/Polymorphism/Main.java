d10cc212e8476c287647d5ae0b7dd9d36dcbcdbe
samikshyaghimire <samikshyaghimire2058@gmail.com>
2026-08-06 18:41:33 +0900

    some more practices

--- src/Polymorphism/Main.java
+++ src/Polymorphism/Main.java
@@ -7,6 +7,14 @@ public class Main {
         //              object can identify as other objects
         //              Objects can be treated as objects of a common superclass
 
+        Car car = new Car();
+        Bike bike = new Bike();
+        Boat boat = new Boat();
 
+       Vehicle[] vehicles ={car,bike,boat};
+
+       for(Vehicle vehicle : vehicles){
+           vehicle.go();
+       }
     }
 }
